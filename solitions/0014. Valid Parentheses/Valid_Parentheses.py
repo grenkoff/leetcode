09a691cbf931c6e4308897a10dc766425a8a4d31
Alexander Grenkov <a.grenkov@gmail.com>
2022-11-12 00:21:37 +0300

    Create Valid_Parentheses.py

---
+++ solitions/0014. Valid Parentheses/Valid_Parentheses.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {')': '(', ']': '[', '}': '{'}
+        for i in s:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
