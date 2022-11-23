59828404f15ab0a133c62555b3d53c35ba3eb426
Alexander Grenkov <a.grenkov@gmail.com>
2022-11-24 02:01:50 +0300

    Create Missing_Number.py

---
+++ solitions/0268. Missing Number/Missing_Number.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/missing-number/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        numsLen = len(nums)
+        xlength = nums[numsLen-1] - nums[0] + 1
+        xsum = int(nums[numsLen-1] * (nums[numsLen-1] + 1) / 2 - nums[0] * (nums[0] - 1) / 2)
+        if nums[0] != 0:
+            return 0
+        else:
+            if numsLen != xlength:
+                return xsum - sum(nums)
+            else:
+                return nums[len(nums)-1] + 1
