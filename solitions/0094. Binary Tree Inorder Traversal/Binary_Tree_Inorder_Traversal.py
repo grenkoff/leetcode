2f1c9ea25baf5e9dd8a4e9fb618c5e272ad4f738
Alexander Grenkov <a.grenkov@gmail.com>
2022-11-24 13:39:29 +0300

    Create Binary_Tree_Inorder_Traversal.py

---
+++ solitions/0094. Binary Tree Inorder Traversal/Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Iterative Approach
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+    
+    # Recursive Approach
+    def recursiveApproach(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
+      
+      # Morris Traversal
