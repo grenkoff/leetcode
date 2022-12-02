164c41ab3353fbcc34c405483af90e297132f177
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-03 01:34:45 +0300

    Update Valid_Anagram.py

--- solitions/0242. Valid Anagram/Valid_Anagram.py
+++ solitions/0242. Valid Anagram/Valid_Anagram.py
@@ -5,19 +5,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if sorted(s) == sorted(t):
             return True
-        else:
-            return False
+        return False
 
 
     # Approach using str.count()
+    # Time: O(n)
     def isAnagram(self, s: str, t: str) -> bool:
-        flag = True
         if len(s) != len(t): 
-            flag = False
-        else:
-            letters = "abcdefghijklmnopqrstuvwxyz"
-            for letter in letters:
-                if s.count(letter) != t.count(letter):
-                    flag = False
-                    break
-        return flag
+            return False
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        for letter in letters:
+            if s.count(letter) != t.count(letter):
+                return False
+        return True
