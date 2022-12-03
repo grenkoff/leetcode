4117d20854416c28e9c49ce1e01cf2fedb367f66
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-04 00:38:28 +0300

    Update Single_Number.py

--- solitions/0136. Single Number/Single_Number.py
+++ solitions/0136. Single Number/Single_Number.py
@@ -2,10 +2,12 @@
 
 class Solution:
     # Brute Force
+    # Time: O(n**2), Space: O(1)
     def bruteForce(self, nums: List[int]) -> int:
         return sum(list(set(nums)) * 2) - sum(nums)
     
     # Use Sorting
+    # Time: O(nlogn) for sorting then O(n) to check neighbouring elements, Space: O(1)
     def sorting(self, nums: List[int]) -> int:
         nums.sort()
         stack=[]
@@ -19,6 +21,7 @@ class Solution:
         return stack[0]
     
     # Use Hashing/Set
+    # Time: O(n), Space: O(n)
     def hashing(self, nums: List[int]) -> int:
         counts = {}
         for i in nums:
@@ -29,6 +32,7 @@ class Solution:
         return list(counts.keys())[0]
     
     # Bit Manipulation
+    # Time: O(n), Space: O(1)
     def bitManipulation(self, nums: List[int]) -> int:
         xor = 0
         for num in nums:
