220357a1d0431f1c090f4f4fadd4259720dec270
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-04 05:38:01 +0300

    Update Sqrt(x).py

--- solitions/0069. Sqrt(x)/Sqrt(x).py
+++ solitions/0069. Sqrt(x)/Sqrt(x).py
@@ -1,7 +1,8 @@
 # https://leetcode.com/problems/sqrtx/
 
-# Binary Search. O(log n)
 class Solution:
+    # Binary Search
+    # Time: O(logn), Space: O(?)
     def mySqrt(self, x: int) -> int:
         if x <= 1:
             return x
@@ -15,3 +16,11 @@ class Solution:
             else:
                 right = mid - 1
         return right
+    
+    # Newton-Raphson method (read more: https://leetcode.com/problems/sqrtx/discuss/2732386/O(1)-solution-in-python!(Newton-Raphson-method))
+    # Time: O(1), Space: O(?)
+    def newtonRaphson(self, x: int) -> int:
+        ans = 1.0
+        for i in range(20):
+            ans = ans - (ans * ans - x)/(2 * ans)
+        return int(ans)
