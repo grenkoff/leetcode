b97ce1c3a3399c010efae8ef82b1f2fb13f834ce
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-10 23:42:18 +0300

    Update Palindrome_Linked_List.py

--- solitions/0234. Palindrome Linked List/Palindrome_Linked_List.py
+++ solitions/0234. Palindrome Linked List/Palindrome_Linked_List.py
@@ -6,66 +6,71 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Array Solution :: Space: O(n)
+    # Array Solution
+    # Time: O(n), Space: O(n)
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
-        while head:
-            nums.append(head.val)
-            head = head.next
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            if nums[left] != nums[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-    
-    # Two Pointers Solution :: Space: O(1)
-    def twoPointers(self, head: Optional[ListNode]) -> bool:
-        fast = head
-        slow = head
-        
-        # find middle (slow)
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            
-        # reverse second half
-        prev = None
-        while slow:
-            tmp = slow.next
-            slow.next = prev
-            prev = slow
-            slow = tmp
-        
-        # check palindrome
-        left, right = head, prev
-        while right:
-            if left.val != right.val:
-                return False
-            left = left.next
-            right = right.next
-        return True
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
     
     # Recursive Solution
-    def recursiveSolution(self, head: Optional[ListNode]) -> bool:
-        global front
-        front = head
+    # Time: O(n), Space: O(n)
+    def recursiveSolution(self, head: ListNode) -> bool:
+    
+        self.front_pointer = head
 
-        def helper(back) -> bool:
-            global front
-            if not back:
-                return True
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
 
-            #let back pointer travel to the back of the list through recursion
-            equal_so_far = helper(back.next)
+        return recursively_check()
+    
+    # Reverse Second Half In-place
+    # Time: O(n), Space: O(1)
+    def reverseSecondHalf(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return True
 
-            #check if front and back have the same value
-            value_equal = (front.val == back.val)
+        # Find the end of first half and reverse second half.
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
 
-            #when the function return, back is gradually moved toward head of the list
-            #move front accordingly to compare their value
-            front = front.next
-            return equal_so_far and value_equal
-        
-        return helper(head)
+        # Check whether or not there's a palindrome.
+        result = True
+        first_position = head
+        second_position = second_half_start
+        while result and second_position is not None:
+            if first_position.val != second_position.val:
+                result = False
+            first_position = first_position.next
+            second_position = second_position.next
+
+        # Restore the list and return the result.
+        first_half_end.next = self.reverse_list(second_half_start)
+        return result    
+
+    def end_of_first_half(self, head):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def reverse_list(self, head):
+        previous = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        return previous
