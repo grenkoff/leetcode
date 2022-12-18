e18fcb7bb463e07ae14c67bd192e1029ffec88e8
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-19 00:48:34 +0300

    Create Generate_Parentheses.py

---
+++ solitions/0022. Generate Parentheses/Generate_Parentheses.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/generate-parentheses/
+
+class Solution:
+    # Brute Force
+    # Time: O(2**2n * n), Space: O(2**2n * n)
+    def bruteForce(self, n: int) -> List[str]:
+        def generate(A = []):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            for c in A:
+                if c == '(': bal += 1
+                else: bal -= 1
+                if bal < 0: return False
+            return bal == 0
+
+        ans = []
+        generate()
+        return ans
+
+      
+    # Backtracking
+    # Time: O(4**n / n**0.5), Space: O(4**n / n**0.5)
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(S = [], left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append("".join(S))
+                return
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
+        return ans
+    
+    
+    # Closure Number
+    # Time: O(4**n / n**0.5), Space: O(4**n / n**0.5)
+    def closureNumber(self, N):
+        if N == 0: return ['']
+        ans = []
+        for c in range(N):
+            for left in self.generateParenthesis(c):
+                for right in self.generateParenthesis(N-1-c):
+                    ans.append('({}){}'.format(left, right))
+        return ans
