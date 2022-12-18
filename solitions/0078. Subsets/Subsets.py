7e22b7bd206bb22b16cd29779e093468bf7bb6ec
Alexander Grenkov <a.grenkov@gmail.com>
2022-12-19 00:16:01 +0300

    Update Subsets.py

--- solitions/0078. Subsets/Subsets.py
+++ solitions/0078. Subsets/Subsets.py
@@ -1,11 +1,52 @@
 # https://leetcode.com/problems/subsets/
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    # Cascading
+    # Time: O(n * 2**n), Space: O(n * 2**n)
+    def cascading(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         output = [[]]
         
         for num in nums:
             output += [curr + [num] for curr in output]
+        
+        return output
+
+    
+    # Backtracking
+    # Time: O(n * 2**n), Space: O(n)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
+    
+    
+    # Lexicographic (Binary Sorted) Subsets
+    # Time: O(n * 2**n), Space: O(n * 2**n)
+    def lexicographic(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        output = []
+        
+        for i in range(2**n, 2**(n + 1)):
+            # generate bitmask, from 0..00 to 1..11
+            bitmask = bin(i)[3:]
+            
+            # append subset corresponding to that bitmask
+            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
         
         return output
