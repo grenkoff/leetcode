e267d48af38585dbaa5b32e0668ed0d01c906599
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-11 02:22:44 +0600

    add O(m * n) solution

--- solitions/0049. Group Anagrams/Group_Anagrams.py
+++ solitions/0049. Group Anagrams/Group_Anagrams.py
@@ -1,1 +1,27 @@
 # https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    '''HashMap Solution
+    Time: O(m * n), m is the number of strings we`re given,
+    n is the average length of each string'''
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        from collections import defaultdict
+
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            res[tuple(count)].append(s)
+
+        return res.values()
+
+
+a = Solution()
+
+print(a.groupAnagrams(["eat","tea","tan","ate","nat","bat"])) # dict_values([['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']])
+print(a.groupAnagrams([""])) # dict_values([['']])
+print(a.groupAnagrams(["a"])) # dict_values([['a']])
