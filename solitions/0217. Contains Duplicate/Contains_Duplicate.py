9002ae592ddcc2ff4cea83a27a8b339812031458
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-10 21:49:33 +0600

    add O(n) solution

--- solitions/0217. Contains Duplicate/Contains_Duplicate.py
+++ solitions/0217. Contains Duplicate/Contains_Duplicate.py
@@ -2,5 +2,21 @@
 
 class Solution:
     # One Liner Brute Force
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+    # def containsDuplicate(self, nums: List[int]) -> bool:
+    #     return len(set(nums)) != len(nums)
+
+    
+    # Time: O(n), Space: O(n)
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+
+a = Solution()
+
+print(a.containsDuplicate([1, 2, 3, 1]))
+print(a.containsDuplicate([1, 2, 3, 4]))
