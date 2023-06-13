7f434386fd6a122c15eb5641345a22f333c73d94
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-13 20:25:17 +0600

    add NeetCode Solution

--- solitions/0036. Valid Sudoku/Valid_Sudoku.py
+++ solitions/0036. Valid Sudoku/Valid_Sudoku.py
@@ -13,6 +13,29 @@ class Solution:
         return True
 
 
+    # Neetcode Solution
+    # def isValidSudoku(self, board: List[List[str]]) -> bool:
+    #     cols = collections.defaultdict(set)
+    #     rows = collections.defaultdict(set)
+    #     squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+    #     for r in range(9):
+    #         for c in range(9):
+    #             if board[r][c] == ".":
+    #                 continue
+    #             if (
+    #                 board[r][c] in rows[r]
+    #                 or board[r][c] in cols[c]
+    #                 or board[r][c] in squares[(r // 3, c // 3)]
+    #             ):
+    #                 return False
+    #             cols[c].add(board[r][c])
+    #             rows[r].add(board[r][c])
+    #             squares[(r // 3, c // 3)].add(board[r][c])
+
+    #     return True
+
+
 a = Solution()
 
 board = [["5","3",".",".","7",".",".",".","."]
