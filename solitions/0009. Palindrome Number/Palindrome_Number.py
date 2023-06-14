9e90d4854bc5028e6cfa1710f03a703c6edf21f0
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-14 19:53:58 +0600

    Delete solitions directory

--- solitions/0009. Palindrome Number/Palindrome_Number.py
+++
@@ -1,6 +0,0 @@
-# https://leetcode.com/problems/palindrome-number/
-
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        s = str(x)
-        return s == s[::-1]
--- solitions/0066. Plus One/Plus_One.py
+++
@@ -1,5 +0,0 @@
-# https://leetcode.com/problems/plus-one/
-
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        return list(str(int("".join([str(i) for i in digits])) + 1))
