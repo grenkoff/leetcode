0e59eeb3a1cf41f1cefb15d6ef7671eaa1c2b8b5
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-16 16:58:13 +0600

    rename file and add testcases

--- solutions/0167. Two Sum II - Input Array Is Sorted/Two_Sum_II_-_Input_Array_Is_Sorted.py
+++ solutions/0167. Two Sum II - Input Array Is Sorted/Two_Sum_II_-_Input_Array_Is_Sorted.py
@@ -6,7 +6,7 @@ class Solution:
 
     # Two Pointers Solution
     # Time: O(n), Space: O(1)
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
         l, r = 0, len(numbers) - 1
 
         while l < r:
@@ -17,3 +17,10 @@ class Solution:
                 l += 1
             else:
                 return [l + 1, r + 1]
+
+a = Solution()
+
+print(a.twoSum([1, 3, 4, 5, 7, 10, 11], 9)) # [3, 4]
+print(a.twoSum([2, 7, 11, 15], 9)) # [1, 2]
+print(a.twoSum([2, 3, 4], 6)) # [1, 3]
+print(a.twoSum([-1, 0], -1)) # [1, 2]
