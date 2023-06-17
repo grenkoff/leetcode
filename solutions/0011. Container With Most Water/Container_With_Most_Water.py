9363a7ced35e7e9dd5eba89eb697debfb50056a8
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-17 20:41:45 +0600

    add two pointers solution Time: O(n)

---
+++ solutions/0011. Container With Most Water/Container_With_Most_Water.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    # Two Pointers Solution
+    # Time: O(n)
+    def maxArea(self, height: list[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = 0
+        h = max(height)
+
+        while l < r:
+            max_area = max(max_area, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            
+            if (r-l) * h <= max_area:
+                break 
+        return max_area
+
+
+a = Solution()
+
+print(a.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])) # 49
+print(a.maxArea([1,1])) # 1
+print(a.maxArea([3, 2, 101])) # 6
