340558ca34fe62b08458f1abd078d6734cdfc22e
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-19 03:09:50 +0600

    add message Time: O(n)

--- solutions/0155. Min Stack/Min_Stack.py
+++ solutions/0155. Min Stack/Min_Stack.py
@@ -4,18 +4,22 @@ class MinStack:
         self.minStack = []
 
     def push(self, val: int) -> None:
+        # Time: O(1)
         self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
 
     def pop(self) -> None:
+        # Time: O(1)
         self.stack.pop()
         self.minStack.pop()
 
     def top(self) -> int:
+        # Time: O(1)
         return self.stack[-1]
 
     def getMin(self) -> int:
+        # Time: O(1)
         return self.minStack[-1]
 
 
