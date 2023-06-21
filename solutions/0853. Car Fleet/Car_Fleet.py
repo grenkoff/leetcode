c95853da9ba0d3ea409a424e81b3a4b982adb1a9
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-22 05:16:59 +0600

    add stack-based solution Time: O(nlogn)

---
+++ solutions/0853. Car Fleet/Car_Fleet.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        # Stack-based solution Time: O(nlogn), Space: O(n)
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+
+a = Solution()
+
+assert a.carFleet(12, [10,8,0,5,3], [2,4,1,1,3]) == 3
+assert a.carFleet(10, [3], [3]) == 1
+assert a.carFleet(100, [0, 2, 4], [4, 2, 1]) == 1
