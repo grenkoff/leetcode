cfad727e5303d0938b354b17ce47efacf7ca2fdd
Alexander Grenkov <a.grenkov@gmail.com>
2023-06-22 04:43:35 +0600

    called variables correctly

--- solutions/0739. Daily Temperatures/Daily_Temperatures.py
+++ solutions/0739. Daily Temperatures/Daily_Temperatures.py
@@ -12,14 +12,14 @@ class Solution:
 
         for i, t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = i - stackInd
+                stack_t, stack_ind = stack.pop()
+                res[stack_ind] = i - stack_ind
             stack.append((t, i))
         return res
 
 
 a = Solution()
 
-assert a.dailyTemperatures([73,74,75,71,69,72,76,73]) == [1,1,4,2,1,1,0,0]
-assert a.dailyTemperatures([30,40,50,60]) == [1,1,1,0]
-assert a.dailyTemperatures([30,60,90]) == [1,1,0]
+assert a.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]) == [1, 1, 4, 2, 1, 1, 0, 0]
+assert a.dailyTemperatures([30, 40, 50, 60]) == [1, 1, 1, 0]
+assert a.dailyTemperatures([30, 60, 90]) == [1, 1, 0]
